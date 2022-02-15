a786eb1892af3eaf60c1d7a19dea8d1aba3e9a13
Raju Rangaswami <rangaswami@gmail.com>
2022-02-15 16:47:22 -0500

    Skeleton implementation elevator simulator

--- nachos-3.4/code/threads/elevator.cc
+++ nachos-3.4/code/threads/elevator.cc
@@ -8,32 +8,70 @@ int nextPersonID = 1;
 Lock *personIDLock = new Lock("PersonIDLock");
 
 
-// Elevator *e = new Elevator(numFloors);
+ELEVATOR *e;
 
+
+void ELEVATOR::start() {
+
+    while(1) {
+
+        // A. Wait until hailed
+
+        // B. While there are active persons, loop doing the following
+        //      0. Acquire elevatorLock
+        //      1. Signal persons inside elevator to get off (leaving->broadcast(elevatorLock))
+        //      2. Signal persons atFloor to get in, one at a time, checking occupancyLimit each time
+        //      2.5 Release elevatorLock
+        //      3. Go to next floor
+        //  printf("Elevator arrives on floor %d", )
+    }
+}
 
 void ElevatorThread(int numFloors) {
 
     printf("Elevator with %d floors was created!\n", numFloors);
 
-    // DO the following  steps A and B forever ...
+    e = new ELEVATOR(numFloors);
 
-    // A. Wait until hailed
+    e->start();
 
-    // B. While there are active persons, loop doing the following
-    //      1. Signal persons inside elevator to get off
-    //      2. Signal persons atFloor to get in
-    //      3. Go to next floor
-    //  printf("Elevator arrives on floor %d", )
 
 }
 
+ELEVATOR::ELEVATOR(int numFloors) {
+    entering = new Condition*[numFloors];
+    // Initialize entering
+    for (int i = 0; i < numFloors; i++) {
+        entering[i] = new Condition("Entering " + i);
+    }
+    personsWaiting = new int[numFloors];
+    elevatorLock = new Lock("ElevatorLock");
+
+    // Initialize leaving
+}
 
 
 void Elevator(int numFloors) {
     // Create Elevator Thread
     Thread *t = new Thread("Elevator");
     t->Fork(ElevatorThread, numFloors);
+}
 
+
+void ELEVATOR::hailElevator(Person *p) {
+    // 1. Increment waiting persons atFloor
+    // 2. Hail Elevator
+    // 2.5 Acquire elevatorLock;
+    // 3. Wait for elevator to arrive atFloor [entering[p->atFloor]->wait(elevatorLock)]
+    // 5. Get into elevator
+    printf("Person %d got into the elevator.\n", p->id);
+    // 6. Decrement persons waiting atFloor [personsWaiting[atFloor]++]
+    // 7. Increment persons inside elevator [occupancy++]
+    // 8. Wait for elevator to reach toFloor [leaving[p->toFloor]->wait(elevatorLock)]
+    // 9. Get out of the elevator
+    printf("Person %d got out of the elevator.\n", p->id);
+    // 10. Decrement persons inside elevator
+    // 11. Release elevatorLock;
 }
 
 void PersonThread(int person) {
@@ -42,14 +80,7 @@ void PersonThread(int person) {
 
     printf("Person %d wants to go from floor %d to %d\n", p->id, p->atFloor, p->toFloor);
 
-    // Increment waiting persons atFloor
-    // Hail Elevator
-    // Wait for elevator to arrive atFloor
-    // Get into elevator
-    printf("Person %d got into the elevator.\n", p->id);
-    // Wait for elevator to reach toFloor
-    // Get out of the elevator
-    printf("Person %d got out of the elevator.\n", p->id);
+
 
 }
 
