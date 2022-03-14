bf60b22925f124b1c427ebe82ee4fed9801fd5b3
Raju Rangaswami <rangaswami@gmail.com>
2022-03-14 12:28:24 -0400

    AddrSpace clone

--- nachos-3.4/code/userprog/addrspace.cc
+++ nachos-3.4/code/userprog/addrspace.cc
@@ -119,9 +119,14 @@ AddrSpace::AddrSpace(OpenFile *executable)
 }
 
 
+TranslationEntry* AddrSpace::GetPageTable() {
+    return pageTable;
+}
+
 unsigned int AddrSpace::GetNumPages() {
     return numPages;
 }
+
 
 //----------------------------------------------------------------------
 // AddrSpace::AddrSpace
@@ -131,14 +136,35 @@ unsigned int AddrSpace::GetNumPages() {
 AddrSpace::AddrSpace(AddrSpace* space) {
 
     // 1. Find how big the source address space is
+    unsigned int n = space->GetNumPages();
+
+    // Acquire mmLock
+    mmLock->Acquire();
 
     // 2. Check if there is enough free memory to make the copy. IF not, fail
+    ASSERT(n <= mm->GetFreePageCount());
 
     // 3. Create a new pagetable of same size as source addr space
+    pageTable = new TranslationEntry[n];
 
     // 4. Make a copy of the PTEs but allocate new physical pages
+    TranslationEntry* ppt = space->GetPageTable();
+    for (int i = 0; i < numPages; i++) {
+        pageTable[i].virtualPage = ppt[i].virtualPage;
+        pageTable[i].physicalPage = mm->AllocatePage();
+        pageTable[i].valid = ppt[i].valid;
+        pageTable[i].use = ppt[i].use;
+        pageTable[i].dirty = ppt[i].dirty;
+        pageTable[i].readOnly = ppt[i].readOnly;
 
-    // 5. For each page, make an actual copy of the contents of the page
+        // 5. For each page, make an actual copy of the contents of the page
+        bcopy(  &(machine->mainMemory[ppt[i].physicalPage*128]),
+                &(machine->mainMemory[pageTable[i].physicalPage*128]),
+                128);
+    }
+
+    // Release mmLock
+    mmLock->Release();
 
 }
 
