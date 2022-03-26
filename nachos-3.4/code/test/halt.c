5145847861424fd164a0d78f2e2c76b63c3dc2f3
Leandro Estevez Diaz <leoest.dev@gmail.com>
2022-03-26 12:32:53 -0400

    fork system call

--- nachos-3.4/code/test/halt.c
+++ nachos-3.4/code/test/halt.c
@@ -1,20 +1,58 @@
 /* halt.c
  *	Simple program to test whether running a user program works.
- *	
+ *
  *	Just do a "syscall" that shuts down the OS.
  *
- * 	NOTE: for some reason, user programs with global data structures 
+ * 	NOTE: for some reason, user programs with global data structures
  *	sometimes haven't worked in the Nachos environment.  So be careful
- *	out there!  One option is to allocate data structures as 
+ *	out there!  One option is to allocate data structures as
  * 	automatics within a procedure, but if you do this, you have to
  *	be careful to allocate a big enough stack to hold the automatics!
  */
 
 #include "syscall.h"
 
-int
-main()
+// int
+// main()
+// {
+//     Halt();
+//     /* not reached */
+// }
+#include "syscall.h"
+
+int global_cnt=0;
+
+void sum(){
+
+	int i;
+
+	for (i=0;i<100;i++)
+	{
+	    global_cnt++;
+	}
+	Exit(i);
+}
+
+int main()
 {
-    Halt();
-    /* not reached */
+	int i=0;
+
+	global_cnt++;
+
+	Fork(sum);
+	Yield();
+
+
+	global_cnt++;
+	Fork(sum);
+
+	Yield();
+
+	global_cnt++;
+	Fork(sum);
+
+	Yield();
+
+	global_cnt++;
+	Exit(global_cnt);
 }
