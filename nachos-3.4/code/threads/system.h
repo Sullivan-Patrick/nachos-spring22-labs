30e3e7e1c5b2bf73e873054a6147c5a795724282
Raju Rangaswami <rangaswami@gmail.com>
2022-03-28 10:32:12 -0400

    handling circular includes

--- nachos-3.4/code/threads/system.h
+++ nachos-3.4/code/threads/system.h
@@ -22,6 +22,8 @@ extern void Initialize(int argc, char **argv); 	// Initialization,
 extern void Cleanup();				// Cleanup, called when
 						// Nachos is done.
 
+class Thread; // handle circular includes
+
 extern Thread *currentThread;			// the thread holding the CPU
 extern Thread *threadToBeDestroyed;  		// the thread that just finished
 extern Scheduler *scheduler;			// the ready list
