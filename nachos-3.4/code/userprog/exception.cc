38682863a479e979e8513be33ede35bc388ac84c
Leandro Estevez Diaz <leoest.dev@gmail.com>
2022-04-04 20:44:28 -0400

    add exec test and code

--- nachos-3.4/code/userprog/exception.cc
+++ nachos-3.4/code/userprog/exception.cc
@@ -192,7 +192,7 @@ int doExec(char* filename) {
 
     // 2. Create new address space
     // space = new AddrSpace(executable);
-    delete currentThread->space;
+    //delete currentThread->space;
     space = new AddrSpace(executable);
     PCB *currentThreadPcb = pcbManager->AllocatePCB();
     currentThread->space->pcb = currentThreadPcb;
@@ -226,7 +226,7 @@ int doExec(char* filename) {
 
     // 6. Delete current address space
     // delete currentThread->space;
-    // delete currentThread->space;
+    delete currentThread->space;
 
     // 7. Set the addrspace for currentThread
     // currentThread->space = space;
