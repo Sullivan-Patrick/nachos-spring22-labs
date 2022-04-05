66d12afd007f5705fe5ae4762b50922da3fc23f0
Leandro Estevez Diaz <leoest.dev@gmail.com>
2022-04-04 21:02:24 -0400

    add exec test and code

--- nachos-3.4/code/userprog/exception.cc
+++ nachos-3.4/code/userprog/exception.cc
@@ -188,7 +188,7 @@ int doExec(char* filename) {
 
     // 2. Create new address space
     // space = new AddrSpace(executable);
-    //delete currentThread->space;
+    delete currentThread->space;
     space = new AddrSpace(executable);
 
     // 3. Check if Addrspace creation was successful
@@ -219,7 +219,7 @@ int doExec(char* filename) {
 
     // 6. Delete current address space
     // delete currentThread->space;
-    delete currentThread->space;
+    //delete currentThread->space;
 
     // 7. Set the addrspace for currentThread
     // currentThread->space = space;
