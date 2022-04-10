1ed85efe47a94e316c9d9e6aab26964d3b376bf3
Leandro Estevez Diaz <leoest.dev@gmail.com>
2022-04-10 10:10:04 -0400

    add exec test and code

--- nachos-3.4/code/userprog/addrspace.cc
+++ nachos-3.4/code/userprog/addrspace.cc
@@ -114,13 +114,13 @@ AddrSpace::AddrSpace(OpenFile *executable)
 
 // then, copy in the code and data segments into memory
     if (noffH.code.size > 0) {
-        printf("Copying the code");
+        printf("Copying the code\n");
         DEBUG('a', "Initializing code segment, at 0x%x, size %d\n",
 			noffH.code.virtualAddr, noffH.code.size);
         executable->ReadAt(&(machine->mainMemory[noffH.code.virtualAddr]),
 			noffH.code.size, noffH.code.inFileAddr);
 
-        printf("Copying the code, %d", machine->mainMemory[noffH.code.virtualAddr]);
+        printf("Copying the code, %d\n", machine->mainMemory[noffH.code.virtualAddr]);
     }
     if (noffH.initData.size > 0) {
         printf("Copying the initialized data");
