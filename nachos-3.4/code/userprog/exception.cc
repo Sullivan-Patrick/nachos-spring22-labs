a91b52fde5710cd424918d2f9247acc9106fba91
Leandro Estevez Diaz <leoest.dev@gmail.com>
2022-04-10 11:02:30 -0400

    kill system call

--- nachos-3.4/code/userprog/exception.cc
+++ nachos-3.4/code/userprog/exception.cc
@@ -280,19 +280,37 @@ int doKill (int pid) {
     // 1. Check if the pid is valid and if not, return -1
     // PCB* joinPCB = pcbManager->GetPCB(pid);
     // if (pcb == NULL) return -1;
+    PCB* joinPCB = pcbManager->GetPCB(pid);
+    if (joinPCB == NULL) return -1;
 
     // 2. IF pid is self, then just exit the process
     // if (pcb == currentThread->space->pcb) {
     //         doExit(0);
     //         return 0;
     // }
+    if (joinPCB == currentThread->space->pcb) {
+        doExit(0);
+        return 0;
+    }
 
     // 3. Valid kill, pid exists and not self, do cleanup similar to Exit
     // However, change references from currentThread to the target thread
     // pcb->thread is the target thread
+    joinPCB->thread->space;
+    delete joinPCB->thread->space;
+    joinPCB->thread->Finish();
+
+    // Manage PCB memory As a parent process
+    PCB* pcb = joinPCB;
+
+    // Delete exited children and set parent null for non-exited ones
+    pcb->DeleteExitedChildrenSetParentNull();
+
+    // Manage PCB memory As a child process
+    if(pcb->parent == NULL) pcbManager->DeallocatePCB(pcb);
 
     // 4. return 0 for success!
-
+    return 0;
 }
 
 
