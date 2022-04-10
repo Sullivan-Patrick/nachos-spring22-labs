4b3665e43e5521c6d2921b09fe9448c231a6d060
Leandro Estevez Diaz <leoest.dev@gmail.com>
2022-04-10 13:06:58 -0400

    join system call

--- nachos-3.4/code/userprog/exception.cc
+++ nachos-3.4/code/userprog/exception.cc
@@ -255,7 +255,7 @@ int doJoin(int pid) {
     // PCB* joinPCB = pcbManager->GetPCB(pid);
     // if (pcb == NULL) return -1;
     PCB* joinPCB = pcbManager->GetPCB(pid);
-    printf("joinPCB id %d\n", joinPCB->pid);
+    // printf("joinPCB id %d\n", joinPCB->pid);
     if (joinPCB == NULL) {
         printf("process already done \n");
         return -1;
