849bf30e8744bb1c55b17be16944677033e882f5
Leandro Estevez Diaz <leoest.dev@gmail.com>
2022-04-16 11:43:56 -0400

    print statements

--- nachos-3.4/code/userprog/pcbmanager.cc
+++ nachos-3.4/code/userprog/pcbmanager.cc
@@ -5,7 +5,6 @@
 PCBManager::PCBManager(int maxProcesses) {
 
     bitmap = new BitMap(maxProcesses);
-    printf("bitmap built with %d bits\n", maxProcesses);
     pcbs = new PCB*[maxProcesses];
 
     for(int i = 0; i < maxProcesses; i++) {
